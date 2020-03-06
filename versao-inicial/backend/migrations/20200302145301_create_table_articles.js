b2ef5805f0c442ea2047c3e3fe6977b453ba3e29
Alexandre <alexandre.azevedo.mrus@gmail.com>
2020-03-06 16:29:10 -0300

    Subindo projeto blog

---
+++ versao-inicial/backend/migrations/20200302145301_create_table_articles.js
@@ -0,0 +1,16 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('articles', table => {
+    table.increments('id').primary()
+    table.string('name').notNull()
+    table.string('description', 1000).notNull()
+    table.string('imageurl', 1000)
+    table.binary('content').notNull()
+    table.integer('userID').references('id').inTable('users')
+    table.integer('categoryId').references('id').inTable('categories').notNull()
+  })
+
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('articles')
+}
