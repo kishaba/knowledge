40dda6ab5aafc53468f3f3aebbe15faec5e79497
Alexandre <alexandre.azevedo.mrus@gmail.com>
2020-03-10 07:52:37 -0300

    arvore

--- versao-inicial/backend/api/teste.js
+++ versao-inicial/backend/api/teste.js
@@ -1,11 +1,19 @@
 
 const a = [1,2,3]
+const b = [2,3,4]
 
 
 
-let parent = a.splice(i => i != 3)
+let parent = a.map(function(value,indice ) {
+  return value*b[indice]
+}); 
+
+let parent2 = a.map((value,indice ) => value*b[indice])
+
+
 
 
 console.log(parent)
+console.log(parent2)
 
 
