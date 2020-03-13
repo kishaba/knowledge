0c1873e41d56fb52eca41fc80a6cc849faafdafd
Alexandre <alexandre.azevedo.mrus@gmail.com>
2020-03-13 16:30:34 -0300

    versão final 1.0 backend

--- versao-inicial/backend/config/mongodb.js
+++ versao-inicial/backend/config/mongodb.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true, useUnifiedTopology: true })
+const { mongoDBURL } = require('../.env')
+
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then (_ =>  {
     msg = 'MongoDB Conectado com sucesso!' 
     console.log('\x1b[44m%s\x1b[37m', msg, '\x1b[0m')
-  
   })
   .catch(e => {
     const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
