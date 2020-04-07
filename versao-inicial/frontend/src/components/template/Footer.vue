fd60556c99ce37e3f61c84dc937d9f583bf79a9d
Alexandre <alexandre.azevedo.mrus@gmail.com>
2020-04-07 16:56:14 -0300

    Iniciando css grid
    Iniciando css grid

---
+++ versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Disconnect</strong> © 2020</span>
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+
+}
+</script>
+
+<style>
+  .footer {
+    grid-area: footer;
+    background-color: #ddd;
+    color: #333;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right:10px;
+  }
+</style>
